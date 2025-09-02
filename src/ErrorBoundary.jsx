50196b782590a999ccdaae1b4b2126445bc1f904
aliyakundil <aliya1998n@gmail.com>
2025-09-02 20:31:35 +0600

    react router

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,25 @@
+// ErrorBoundary.js
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Log the error to an error reporting service
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong. Please try again later.</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
